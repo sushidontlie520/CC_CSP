1f139cbac654dbf694a01ff20a94ac1761a1665d
sushidontlie520 <charlie.cogswell@ucas-edu.net>
2025-09-03 13:19:56 -0600

    CC_CSP

---
+++ Practices/Variables_practice.py
@@ -0,0 +1,13 @@
+#CC 6th 
+
+name = "Charlie"
+num = "9"
+number = "10"
+breakfast_today = "donught"
+favorite_color = "pink"
+school_name = "UCAS"
+year = "2025"
+eye_color = "brown" 
+age = "14" 
+favorite_subject = "history" 
+print(name, num, number, breakfast_today, favorite_color, school_name, year, eye_color, age, favorite_subject)
