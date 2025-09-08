6dbc38b336cdbb1517db4819279397a485e2728e
sushidontlie520 <charlie.cogswell@ucas-edu.net>
2025-09-08 13:44:17 -0600

    coreates strings notes

--- Notes/string_notes.py
+++ Notes/string_notes.py
@@ -1,3 +1,13 @@
 #CC 6th string notes
 
 print("I did it!")
+
+#examples of strings
+
+first_name = input("What is your first name?"). strip(). title()
+
+last_name
+
+sentence = "The quick brown fox jumps over the lazy dog."
+
+print("Welcome to my program", name, "!") 
