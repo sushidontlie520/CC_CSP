24a15b8f5f35cd1f25605d2a156fff59cc1b485b
dharrmanfam <charlie.cogswell@ucas-edu.net>
2025-09-08 19:04:27 +0000

    creates string notes

---
+++ Practices/hello.world.py
@@ -0,0 +1,6 @@
+#CC th First python program
+name = input("what is your name?")
+print("Hello," + name + "! Nice to meet you")
+
+food = input("What is your favorite food?")
+print(food + " csounds so yummy!")
