8e9b75ba2386006d35b2e9d52bb92c5e0e7c42cb
dharrmanfam <charlie.cogswell@ucas-edu.net>
2025-09-11 03:15:59 +0000

    did silly sentences

---
+++ Practices/silly_sentences.py
@@ -0,0 +1,9 @@
+#CC 6th silly sentences 
+
+# Get user inputs
+animal = input("Enter an animal: ")
+place = input("Enter a place: ")
+food = input("Enter a type of food: ")
+
+# Create and print the silly sentence using all inputs
+print("One day, a " + animal + " went to " + place + " to eat some delicious ")
