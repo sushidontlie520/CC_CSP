2ae15e103fa2756cb708be45a10ea43c73503a2b
dharrmanfam <charlie.cogswell@ucas-edu.net>
2025-09-17 03:04:05 +0000

    updated hello world

---
+++ Practices/hello_world_functions.py
@@ -0,0 +1,17 @@
+#CC 6th Hello world functions 
+
+def hello(name): 
+    print(f"Hello, {name}!") 
+    
+name = input("What is your name?")
+hello(name)
+name = input("What is your name?")
+hello(name)
+name = input("What is your name?")
+hello(name)
+name = input("What is your name?")
+hello(name)
+name = input("What is your name?")
+hello(name)
+name = input("What is your name?")
+hello(name)
