10984d9535814ec631763b22b3e9e32face2ef4f
dharrmanfam <charlie.cogswell@ucas-edu.net>
2025-09-17 03:27:29 +0000

    did financil

---
+++ Practices/financial_fuctions.py
@@ -0,0 +1,39 @@
+# Function to get user input 
+# def get_ user_inputs():
+income = float(input ("What is your monthly income: "))
+rent = float (input(*What is your monthly rent/mortgage: *)
+utilities = Float(input(*What is your monthly utilities: *))
+groceries = float(input("What is your monthly groceries: *))
+transportation = float(input("what is your monthly transportation:
+* Return all the numbers return income, rent, utilities, groceries, transportation
+# Function to calculate percentages and show results
+def calculate percentages(income, rent, utilities, groceries, transport
+# Calculate percent of income for each expense
+rent_percent = (rent / income) • 100
+utilities_percent - (utilities / income) * 100
+groceries_percent = (groceries / income) * 100
+transportation_percent = (transportation / income) • 100
+# Calculate savings (10% of income)
+savings = income * 0,10
+savings_percent = 10
+# Calculate remaining money
+total_expenses = rent + utilities + groceries + transportation + saving
+leftover _money - income - total_expenses
+
+#show the results
+
+groceries percent = (groceries / income) • 100
+transportation percent = (transportation / income) * 100
+# Calculate savings (10% of income)
+savings = incone * 0.10
+savings_percent = 10
+# Calculate remaining money
+total expenses = rent + utilities + groceries + transportation + savings
+leftover_money - incone - total_expenses
+# Show the results
+print("Your monthly income is $" + str(income) + '% of your income."')ation:))) 
+print("Your rent is $" + str(rent) + " and that is " + str (round(rent_percent)) + "% of your income.")
+print("Your utilities are $" + str(utilities) + " and that is " + str(round(utilities percent)) + *% of your income.*)
+print("Your groceries are $" + str(groceries) + " and that is " + str(round (groceries percent)) + "% of your income. ")
+print("Your transportation is $" + str(transportation) + " and that is " + str(round(transportation percent)) + *S of your income. print (*™)
+print("You should save $" + str(round(savings, 2)) + * a month, that is " + str(savings percent) + *% of your income. *)
