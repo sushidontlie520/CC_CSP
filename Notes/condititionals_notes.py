0158b5a661eb43f38db22e0d6967395e0bf2c6a9
sushidontlie520 <charlie.cogswell@ucas-edu.net>
2025-09-17 13:41:26 -0600

    took notes

---
+++ Notes/condititionals_notes.py
@@ -0,0 +1,19 @@
+#CC 6th conditionals notes
+
+num = input("Pick a number between 1 and 100.") 
+num = 6
+#if num < 10: 
+print(f"{num} is a single didgit number") 
+#if num < 100: 
+print(f"{num} is a two digit number") 
+#if num == 6:
+print("That is the winning number!!! congrats!!!")
+
+name = input("What is your name??")
+name = "Baka"
+    
+if name == "Baka":
+  print("You are the teacher!!")
+
+else: 
+  print (f"Hello {name}, you are a student")  
