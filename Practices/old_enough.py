bdbde78d677cb9cca13e7e1b050661066cd1a5ba
sushidontlie520 <charlie.cogswell@ucas-edu.net>
2025-09-18 13:41:00 -0600

    old enough update 1:40

--- Practices/old_enough.py
+++ Practices/old_enough.py
@@ -8,7 +8,7 @@ elif age >= 16:
     print("You are old enough to drive!")
 elif age >= 15:
     print("You are old enough to get a learners permit!")
-elif age >= 5:
+elif age >= 4:
     print("You are old enough to go to school!")
 else:
     print("You are not old enough to go to school.")
