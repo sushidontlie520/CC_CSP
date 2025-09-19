f4c2ba7775e38bfbb6db6266b63e6b1aa2b90030
sushidontlie520 <charlie.cogswell@ucas-edu.net>
2025-09-19 13:43:28 -0600

    worked

---
+++ Notes/for_lopps.py
@@ -0,0 +1,14 @@
+#CC 6th Time and for loops notes!
+
+import datetime
+
+current = datetime.datetime.now()
+hour = current.hour
+
+#print(f"the time in seconds since jan 1, 1970: {eopch}")")
+print(f"the current time is: {current}")
+print(f"The hour is: {hour}") 
+
+#lists
+
+siblings = ["Sophie", "Sophe", "sophiee", ["Phofie"]]
