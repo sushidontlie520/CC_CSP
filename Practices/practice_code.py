a0623578ba2475ca95a413d79cdeb80941c1490b
dharrmanfam <charlie.cogswell@ucas-edu.net>
2025-09-26 06:33:30 +0000

    erfewfw

---
+++ Practices/practice_code.py
@@ -0,0 +1,31 @@
+#CC 6th practice
+
+Class = input(“Hello") (player, to play D&D you have 3 character choices: Mage, Monk, or Barbarian. Please pick one.”) 
+
+Print(“The mage has magical abilities, the monk has martial arts abilities and the barbarian with strength.”) 
+
+If mage = selected:
+  print(“Good choice!”) 
+
+elif monk = selected 
+  Print(“Good choice!”) 
+
+elif barbarian = selected 
+  Print(“Good choice!”)
+
+else:
+   Print(“Please select a character to play!”)
+
+Break 
+
+If mage = selected:
+    Print(“To begin your quest please select the path you want to go through your options are either the___ or the ____”)
+
+elif monk = selected 
+    Print(“Now that you started your mission please select a path, your options are___ or_____.”)
+
+elif barbarian = selected 
+     Print(“Your path choices are ____ or ____”)
+
+else:
+    Print(“Sorry, that is not a correct option.")
