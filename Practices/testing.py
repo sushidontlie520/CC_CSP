13bcccac260982a633330eff9cc4117c73465a70
dharrmanfam <charlie.cogswell@ucas-edu.net>
2025-10-01 01:18:40 +0000

    hjvhjhjgg

--- Practices/testing.py
+++ Practices/testing.py
@@ -1,37 +1,106 @@
-# ME CC SN SPC 6th Tic Tac Toe
+# ME CC SN SP 6th Tic Tac Toe
 
 import random
 # Variables
-user_input = input("Pick either O or x for a Tic Tac Toe game!: ").capitalize()
+user_input = input("Hey welcome to our tic tac toe game please choose X or O please!").capitalize() 
+numbers_avaliable = [1,2,3,4,5,6,7,8,9]
+turns = 1
+board = ["-","-","-","-","-","-","-","-","-"]
+game_end = False 
+ai_choice = "0"
+player_choice = "0"
+if user_input == "X":
+    ai_choice = "O"
+elif user_input == "O":
+    ai_choice = "X"
 
-if user_input == "x":
-    print("You picked x!")
+
+#User input Max
+if user_input == "X":
+    print("You picked X!")
 elif user_input == "O":
     print("You picked O!")
 else:
-    print("You picked neither O or x, please pick O or x PLEASE!")
+    print("You picked neither O or X, please pick O or X PLEASE!")
 
-board = [" " for _ in range(9)] 
-#display the current board
-def print_board(): 
-       print() 
-       print(f"{board[0]} | {board[1]} | {board[2]}")
-       print("---|---|---") 
-       print(f" {board[3]} | {board[4]} | {board[5]}")
-       print("---|---|---") 
-       print(f"{board[6]} | {board[7]} | {board[8]}")
-       print() 
+#Tic tac toe Board Santi
 
-#winning combos
+def game_board():
+    print(board[0] + "|" + board[1] + "|" + board[2])
+    print("-----")
+    print(board[3] + "|" + board[4] + "|" + board[5])
+    print("-----")
+    print(board[6] + "|" + board[7] + "|" + board[8])
+
+def change_board(num, turn):
+    num = num - 1
+    if num == 0:
+        board[0] = turn    
+    elif num == 1:
+        board[1] = turn
+    elif num == 2:
+        board[2] = turn
+    elif num == 3:
+        board[3] = turn
+    elif num == 4:
+        board[4] = turn
+    elif num == 5:
+        board[5] = turn
+    elif num == 6:
+        board[6] = turn
+    elif num == 7:
+        board[7] = turn
+    elif num == 8:
+        board[8] = turn
+
+#winning combos Charlie 
 def winner_combos(player): 
-    win_combos = []
-    [0,1,2], [3,4,5], [6,7,8],  #if_row
-    [0,4,8], [2,4,6], #if_diagonal
-    [0,3,6], [1,4,7], [2,5,8] #if_column
+    win_combos = [
+        [0,1,2], [3,4,5], [6,7,8],  # rows
+        [0,3,6], [1,4,7], [2,5,8],  # columns
+        [0,4,8], [2,4,6]            # diagonals
+    ]
     for combo in win_combos:
-            if all (board[i] == player for i in combo):
-                return True 
-            return False 
-def is_full():
-            return " " not in board 
-                
+        if all(board[i] == player for i in combo):
+            return True
+    return False
+    
+    
+while game_end == False: 
+    if turns % 2 == 1 and user_input == "X":
+        game_board()
+        player_choice = int(input("Choose a number from 1 to 9?"))
+        if 1 <= player_choice <= 9 and player_choice in numbers_avaliable:
+            change_board(player_choice, user_input) 
+            numbers_avaliable.remove(player_choice)
+            if winner_combos(user_input):
+                game_board()
+                print("You win!")
+                game_end = True
+            turns += 1
+    elif turns % 2 == 1 and user_input == "O":
+        game_board()
+        player_choice = int(input("Choose a number from 1 to 9?"))
+        if 1 <= player_choice <= 9 and player_choice in numbers_avaliable: 
+            change_board(player_choice, user_input)
+            numbers_avaliable.remove(player_choice)
+            if winner_combos(user_input):
+                game_board()
+                print("You win!")
+                game_end = True
+            turns += 1
+        
+    else:
+        game_board()
+        ai_pick = random.choice(numbers_avaliable)
+        change_board(ai_pick, ai_choice)
+        numbers_avaliable.remove(ai_pick)
+        if winner_combos(ai_choice):
+            game_board()
+            print("AI wins!")
+            game_end = True
+        turns +=1
+    if turns > 9: 
+        game_board()
+        print("It's a tie!") 
+        game_end = True 
