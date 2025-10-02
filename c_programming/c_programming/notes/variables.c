22a8111a223165962c7b45415fa4ff8f70e2f980
sushidontlie520 <charlie.cogswell@ucas-edu.net>
2025-10-02 13:43:58 -0600

    got things et u na dstrated notea

---
+++ c_programming/c_programming/notes/variables.c
@@ -0,0 +1,15 @@
+// CC 6th Variables Notes
+#include <stdio.h>
+
+int main(void){
+    int num = 4;
+    float pi = 3.14;
+    char grade = 'A'; //Will only hold one letter
+    char name[]= "Charlie"; 
+    //bool passing = true;
+    printf("%d", num); 
+
+    return 0;
+}
+
+//static is data types for variables don't change num="4" 
