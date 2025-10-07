c78a89a4ec88fa8a14007f3ca660672469c69d7a
dharrmanfam <charlie.cogswell@ucas-edu.net>
2025-10-07 05:20:34 +0000

    did hw 1

--- c_programming/c_programming/practices/firstprogram.c
+++ c_programming/c_programming/practices/firstprogram.c
@@ -1,2 +1,16 @@
 //CC 6th first program in c
 
+#include <stdio.h>
+
+int main(void){
+    //declare variable to store user's name
+    char name[55];
+
+    //ask for the user's name
+    printf("What is your name?: ");
+
+    fgets(name, sizeof(name), stdin);
+
+    printf("Hi %s", name);
+    return 0;
+}
