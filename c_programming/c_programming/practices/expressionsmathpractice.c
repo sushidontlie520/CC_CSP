f12cb40a9667c297c7cbb356a5622588befd5b5e
sushidontlie520 <charlie.cogswell@ucas-edu.net>
2025-10-08 13:39:18 -0600

    started hw bakutoe!!

---
+++ c_programming/c_programming/practices/expressionsmathpractice.c
@@ -0,0 +1,9 @@
+//CC 6th expressions practice
+
+#include <stdio.h>
+#include <math.h> 
+
+int main(void){
+
+    return 0; 
+}
---
+++ c_programming/c_programming/practices/finacialcalculator.c
@@ -0,0 +1,11 @@
+//CC 6th finacial calculator 
+
+#include <stdio.h>
+#include <math.h> 
+
+int main(void){
+    float monthly_income;
+    scanf("%f", &monthly_income);
+    printf("%f", monthly_income);
+    return 0; 
+}
