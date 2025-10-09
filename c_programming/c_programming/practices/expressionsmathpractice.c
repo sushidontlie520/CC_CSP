c69a7fb93a38caa41f5f97753932acd67989de83
dharrmanfam <charlie.cogswell@ucas-edu.net>
2025-10-09 05:04:57 +0000

    gbhh

--- c_programming/c_programming/practices/expressionsmathpractice.c
+++ c_programming/c_programming/practices/expressionsmathpractice.c
@@ -1,9 +1,22 @@
 //CC 6th expressions practice
 
+
 #include <stdio.h>
-#include <math.h> 
+#include <math.h>
 
 int main(void){
+    printf("7-24/8*4+6 = %d\n", (int) 7-24/8*4+6 );
+    printf("18/3-7+2*5 = %d\n", (int) 18/3-7+2*5);
+    printf("6*4/12+72/8-9 = %d\n", (int) 6*4/12+72/8-9);
+    printf("(17-6/2)+4*3 = %d\n", (int) (17-6/2)+4*3);
+    printf("-2(1*4-2/2)+(6+2-3) = %d\n", (int) -2*(1*4-2/2)+(6+2-3));
+    printf("-1*[(3-4*7)/5]-2*24/6 = %d\n", (int) -1*((3-4*7)/5)-2*24/6);
+    printf("(3*5^2/15)-(5-2^2) = %d\n", (3* (int)pow(5,2)/15)-(5- (int)pow(2,2)));
+    printf("(1^4*2^2+3^3)-2^5/4 = %d\n", ((int) pow(1,4)* (int) pow(2,2) + (int) pow(3,3))- (int) pow(2,5)/4);
+    printf("(22/2-2*5)^2 + (4-6/6)^2 = %d\n", (int) pow(22/2-2*5, 2) + (int) pow(4-6/6, 2));
 
-    return 0; 
+
+    return 0;
+}
+ 
 }
