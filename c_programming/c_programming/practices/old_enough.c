150e3f47fa61a143259d196c346c8b8894a6b361
sushidontlie520 <charlie.cogswell@ucas-edu.net>
2025-10-10 13:42:56 -0600

    did other hw

--- c_programming/c_programming/practices/old_enough.c
+++ c_programming/c_programming/practices/old_enough.c
@@ -1,2 +1,24 @@
 //CC 6th old enough practice
 #include <stdio.h>
+#include <string.h>
+ int main(void){
+    int age;
+    char name[1000];
+     printf("What is ur age??: ");
+    scanf("%d", &age);
+
+        if (age >= ) {
+        printf("Good morningg!\n");
+     } else if (time >= 1200) {
+        printf("Good afternoon!\n");
+    } else if (time >= 1800 ) {
+        printf("Good evening!\n");  
+    } else if (time >= 2100 ) {
+        printf("Good night!\n");
+    } else {
+        printf("Please enter a real time.\n");
+    }
+
+
+    return 0; 
+ }
