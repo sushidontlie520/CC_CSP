9ea78c0ce50cfafd59c5d6a22afe76f1aa4c686a
dharrmanfam <charlie.cogswell@ucas-edu.net>
2025-10-10 21:26:56 +0000

    hw

--- c_programming/c_programming/practices/namedeocr.c
+++ c_programming/c_programming/practices/namedeocr.c
@@ -1,1 +1,26 @@
 //CC 6th name decorator 
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[1000];
+    char decoration[] = "<<< ";
+    char end_decoration[] = " >>>";
+    char decorated_name[2000];
+
+    printf("please enter ur name: ");
+    fgets(name, sizeof(name), stdin);
+
+    // Remove newline character from fgets
+    name[strcspn(name, "\n")] = 0;
+
+    // Concatenate decorations and name
+    strcpy(decorated_name, decoration);
+    strcat(decorated_name, name);
+    strcat(decorated_name, end_decoration);
+
+    // Print the decorated name
+    printf("%s\n", decorated_name);
+
+    return 0;
+}
--- c_programming/c_programming/practices/sillysentence.c
+++ c_programming/c_programming/practices/sillysentence.c
@@ -1,9 +1,22 @@
 //CC 6th silly sentnence
 #include <stdio.h>
-#include <string.h>
 
 int main(void){
+    char animal[30];
+    char place[30];
+    char food[30];
 
+    printf("Give me an animal: ");
+    scanf("%s", animal);
+
+    printf("Give me a place: ");
+    scanf("%s", place);
+
+    printf("Give me a food: ");
+    scanf("%s", food);
+
+    printf("one day, a %s went to a %s to eat some delicious %s.\n", animal, place, food);  
+}
 
 
 
