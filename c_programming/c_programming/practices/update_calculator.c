f81f709899705432d3c2d36b40cec1d3ee1500c6
dharrmanfam <charlie.cogswell@ucas-edu.net>
2025-10-17 20:46:56 +0000

    hw

--- c_programming/c_programming/practices/update_calculator.c
+++ c_programming/c_programming/practices/update_calculator.c
@@ -4,41 +4,29 @@
 #include <stdio.h>
 #include <math.h>
 
-float get_number (char* type) {
-float value;
-printf("what is your monthly %s: ", type);
-scanf ("%f", &value);
-return value;
-void print(char* type, float expense, float income) <
-printf("your %s is %.2f which is %.0f%% of your income\n", type, expense, (expense/income) * 100.0);
-}
-int main(void){
-float Income = get_number ("Income");
-float rent = get_number ("rent");
-float utilities = get_ number ("utilities");
-float groceries = get_number ("groceries");
-float transportation = get_ number ("transportation");
-print ("rent", rent, Income);
-print("utilities", utilities, Income);
-print ("groceries", groceries, Income);
-print ("transportation", transportation, Income);
-
-    return 0;
-
-
+float get_number(char* type) {
+    float value;
+    printf("What is your monthly %s: ", type);
+    scanf("%f", &value);
+    return value;
 }
 
-float get_number (char* type){
-    float Income = get_number ("Income");
-float rent = get_number ("rent");
-float utilities = get_ number ("utilities");
-float groceries = get_number ("groceries");
-float transportation = get_ number ("transportation");
-print ("rent", rent, Income);
-print("utilities", utilities, Income);
-print ("groceries", groceries, Income);
-print ("transportation", transportation, Income);
+
+void print(char* type, float expense, float income) {
+    printf("Your %s is %.2f which is %.0f%% of your income\n", type, expense, (expense / income) * 100.0);
+}
+
+int main(void) {
+    float Income = get_number("Income");
+    float rent = get_number("rent");
+    float utilities = get_number("utilities");
+    float groceries = get_number("groceries");
+    float transportation = get_number("transportation");
+
+    print("rent", rent, Income);
+    print("utilities", utilities, Income);
+    print("groceries", groceries, Income);
+    print("transportation", transportation, Income);
 
     return 0;
-
 }
