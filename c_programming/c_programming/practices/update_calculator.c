bbf720aeaa42025f1f58c6349cf450308b2a69f5
dharrmanfam <charlie.cogswell@ucas-edu.net>
2025-10-17 20:35:38 +0000

    did hw

--- c_programming/c_programming/practices/update_calculator.c
+++ c_programming/c_programming/practices/update_calculator.c
@@ -3,36 +3,42 @@
 
 #include <stdio.h>
 #include <math.h>
-    float hello(char* name){
-    float number;
-    printf("what is ur spending on: %s\n", name);
-    scanf("%f", &number);
 
-    return number;
+float get_number (char* type) {
+float value;
+printf("what is your monthly %s: ", type);
+scanf ("%f", &value);
+return value;
+void print(char* type, float expense, float income) <
+printf("your %s is %.2f which is %.0f%% of your income\n", type, expense, (expense/income) * 100.0);
 }
-
-
-int main() {
-    
-    float income, rent, utilities, groceries, transportation;
-    float rent_percent, utilities_percent, groceries_percent, transportation_percent, savings_amount, savings_percent, spending;
-
-    scanf("%f", &transportation);
-
-    rent_percent = (rent/income) * 100;
-    utilities_percent = (utilities/income) * 100;
-    groceries_percent = (groceries/income) * 100;
-    transportation_percent = (transportation/income) * 100;
-    savings_percent = 10;
-    savings_amount = income * (savings_percent/100);
-    spending= income - (rent + utilities + groceries + transportation + savings_amount);
-
-    printf("\nYour rent cost is $%.2f which is %.0f%% of your income.\n", rent, rent_percent);
-    printf("Your utilities cost is $%.2f which is %.0f%% of your income.\n", utilities, utilities_percent);
-    printf("Your groceries cost is $%.2f which is %.0f%% of your income.\n", groceries, groceries_percent);
-    printf("Your transportation cost is $%.2f which %.0f%% of your income.\n", transportation, transportation_percent);
-    printf("\nYou should save $%.2f a month, which is %.0f%% of your income.\n", savings_amount, savings_percent);
-    printf("\nYou have $%.2f of spending money each month!\n", spending);
+int main(void){
+float Income = get_number ("Income");
+float rent = get_number ("rent");
+float utilities = get_ number ("utilities");
+float groceries = get_number ("groceries");
+float transportation = get_ number ("transportation");
+print ("rent", rent, Income);
+print("utilities", utilities, Income);
+print ("groceries", groceries, Income);
+print ("transportation", transportation, Income);
 
     return 0;
+
+
+}
+
+float get_number (char* type){
+    float Income = get_number ("Income");
+float rent = get_number ("rent");
+float utilities = get_ number ("utilities");
+float groceries = get_number ("groceries");
+float transportation = get_ number ("transportation");
+print ("rent", rent, Income);
+print("utilities", utilities, Income);
+print ("groceries", groceries, Income);
+print ("transportation", transportation, Income);
+
+    return 0;
+
 }
